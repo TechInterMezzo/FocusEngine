db1f9922994adc248418da837b21613bc3643518
Phr00t <phr00t@gmail.com>
2021-08-15 15:08:27 -0400

    Restore shader compilation errors in GameStudio

--- sources/editor/Xenko.GameStudio/Logs/BuildLogViewModel.cs
+++ sources/editor/Xenko.GameStudio/Logs/BuildLogViewModel.cs
@@ -14,11 +14,21 @@ namespace Xenko.GameStudio.Logs
             : base(serviceProvider)
         {
         }
-        
+
         public void ForwardSerializableLog(SerializableLogMessage message)
         {
             foreach (var logger in Loggers.Keys)
             {
+                // print out shader errors first
+                if (Xenko.Rendering.EffectSystem.ShaderCompilerErrors.Count > 0)
+                {
+                    foreach (string err in Xenko.Rendering.EffectSystem.ShaderCompilerErrors)
+                    {
+                        logger.Error(err + "\n{{end of shader error history entry}}");
+                    }
+                    Xenko.Rendering.EffectSystem.ShaderCompilerErrors.Clear();
+                }
+
                 logger.Log(message);
             }
         }
